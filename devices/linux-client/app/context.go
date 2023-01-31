e3d59ffbc2b49722604c0216133cacab30fe6ad6
Łukasz Kędziora <lkedziora@antmicro.com>
2023-01-31 10:55:05 +0100

    [#41039] linux-client: app: Reinitialize the database when not loaded
    When building a clean system image with meta-rdfm, there is no database initialized
    at the time of the first boot. As such, the artifact_name and provides are blank.
    One way to initialize the database is to install an update for the first time, but
    in the state described above this becomes impossible - the installer refuses to
    continue when a blank artifact_name is present.
    
    This changes makes RDFM check the database store status when starting - if the DB
    is not yet initialized, a dummy artifact_name is set to allow for installation of
    proper update images.

--- devices/linux-client/app/context.go
+++ devices/linux-client/app/context.go
@@ -8,10 +8,12 @@ import (
 	"github.com/mendersoftware/mender/app"
 	"github.com/mendersoftware/mender/client"
 	"github.com/mendersoftware/mender/conf"
+	"github.com/mendersoftware/mender/datastore"
 	"github.com/mendersoftware/mender/device"
 	"github.com/mendersoftware/mender/installer"
 	"github.com/mendersoftware/mender/store"
 	"github.com/mendersoftware/mender/system"
+	log "github.com/sirupsen/logrus"
 )
 
 type RDFM struct {
@@ -59,12 +61,49 @@ func loadMenderConfig() (*conf.MenderConfig, error) {
 	return menderConfig, nil
 }
 
+// Checks whether the database is initialized by looking at the stored provides
+func isDbStoreInitialized(store *store.DBStore) bool {
+	provides, err := datastore.LoadProvides(store)
+	if err != nil {
+		log.Debug("Provides could not be loaded - database uninitialized")
+		return false
+	}
+
+	// If no provides are present, we assume that the database is uninitialized,
+	// i.e this is the first boot into a system image.
+	if len(provides) > 0 {
+		log.Debug("Database is already initialized", provides)
+		return true
+	}
+
+	log.Debug("Missing provides - database uninitialized")
+	return false
+}
+
+// Reinitializes the client database
+func reinitializeDbStore(s *store.DBStore) (*store.DBStore, error) {
+	log.Debug("Reinitializing database")
+	err := s.WriteTransaction(func(txn store.Transaction) error {
+		return datastore.CommitArtifactData(txn, "unknown", "unknown", make(map[string]string), nil)
+	})
+
+	if err != nil {
+		return nil, errors.New("database initialization failed")
+	}
+	return s, nil
+}
+
 func loadMenderDbStore() (*store.DBStore, error) {
 	store := store.NewDBStore(conf.DefaultDataStore)
 	if store == nil {
 		return nil, errors.New("failed to load DB store")
 	}
-	return store, nil
+
+	if isDbStoreInitialized(store) {
+		return store, nil
+	}
+
+	return reinitializeDbStore(store)
 }
 
 func createMenderDeviceManager(menderConfig *conf.MenderConfig, store *store.DBStore) (*device.DeviceManager, error) {
