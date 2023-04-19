3c1f952cb65af5d54124bc7a765ba8830afa56ed
Łukasz Kędziora <lkedziora@antmicro.com>
2023-04-19 12:44:18 +0200

    [#41944] rdfm-artifact: chore: Minor cleanup, remove unused consts and helpers

--- tools/rdfm-artifact/cli/write_common.go
+++ tools/rdfm-artifact/cli/write_common.go
@@ -4,18 +4,11 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/mendersoftware/mender-artifact/artifact"
-	"github.com/mendersoftware/mender-artifact/awriter"
 	"github.com/urfave/cli"
 )
 
 const (
 	defaultFullArtifactPath = "artifact.rdfm"
-
-	// Data reported in the artifact
-	writerArtifactFormat         = "mender"
-	writerArtifactVersion        = 3
-	writerArtifactFullRootfsType = "rootfs-image"
 
 	// Flag names
 	flagOutputPathName   = "output-path"
@@ -27,10 +20,6 @@ const (
 	flagClearsProvides   = "clears-provides"
 	flagProvides         = "provides"
 	flagDepends          = "depends"
-
-	// Keys for specific values in the provides dictionary
-	providesArtifactNameKey = "artifact_name"
-	providesDeviceTypeKey   = "device_type"
 )
 
 // Flags common across the different artifact modification subcommands
@@ -91,22 +80,6 @@ func parseKeyValuePairs(pairs []string) (map[string]string, error) {
 	return dict, nil
 }
 
-func parseDependsValues(args []string) (artifact.TypeInfoDepends, error) {
-	typeInfoArtifactDepends, err := parseKeyValuePairs(args)
-	if err != nil {
-		return nil, err
-	}
-	return artifact.NewTypeInfoDepends(typeInfoArtifactDepends)
-}
-
-func parseProvidesValues(args []string) (artifact.TypeInfoProvides, error) {
-	typeInfoArtifactProvides, err := parseKeyValuePairs(args)
-	if err != nil {
-		return nil, err
-	}
-	return artifact.NewTypeInfoProvides(typeInfoArtifactProvides)
-}
-
 func parseArtifactName(c *cli.Context) string {
 	return c.String(flagArtifactName)
 }
@@ -147,66 +120,4 @@ func parseOutputPath(c *cli.Context) string {
 		v = defaultFullArtifactPath
 	}
 	return v
-}
-
-// This is a helper for extracting common CLI flags across the write subcommands (write rootfs, write delta)
-// A `WriteArtifactArgs` struct is returned, with the values set from the passed in flags.
-func makeCommonArtifactWriterArgs(c *cli.Context) (*awriter.WriteArtifactArgs, error) {
-	dependsCompatibleDevices := c.StringSlice(flagDeviceType)
-	dependsArtifactName := c.StringSlice(flagDependsArtifacts)
-	dependsArtifactGroup := c.StringSlice(flagDependsGroups)
-
-	providesArtifactName := c.String(flagArtifactName)
-	providesArtifactGroup := c.String(flagProvidesGroup)
-
-	typeInfoType := writerArtifactFullRootfsType
-	typeInfoArtifactDepends, err := parseDependsValues(c.StringSlice(flagDepends))
-	if err != nil {
-		return nil, err
-	}
-	typeInfoArtifactProvides, err := parseProvidesValues(c.StringSlice(flagProvides))
-	if err != nil {
-		return nil, err
-	}
-	typeInfoClearsArtifactProvides := c.StringSlice(flagClearsProvides)
-
-	args := awriter.WriteArtifactArgs{
-		Name:    flagOutputPathName,
-		Format:  writerArtifactFormat,
-		Version: 3,
-		// Artifact depends on one of the following
-		Depends: &artifact.ArtifactDepends{
-			ArtifactName:      dependsArtifactName,
-			CompatibleDevices: dependsCompatibleDevices,
-			ArtifactGroup:     dependsArtifactGroup,
-		},
-		// Artifact provides this name and group
-		Provides: &artifact.ArtifactProvides{
-			ArtifactName:  providesArtifactName,
-			ArtifactGroup: providesArtifactGroup,
-		},
-		// Additional provides
-		// This is for example the rootfs-image.checksum value
-		TypeInfoV3: &artifact.TypeInfoV3{
-			Type:                   &typeInfoType,
-			ArtifactDepends:        typeInfoArtifactDepends,
-			ArtifactProvides:       typeInfoArtifactProvides,
-			ClearsArtifactProvides: typeInfoClearsArtifactProvides,
-		},
-		// What updates are contained within this artifact?
-		// In general, rootfs contain only a single file (the updated rootfs image)
-		// Leaving this empty to be filled by the rootfs/delta writer handlers
-		Updates: &awriter.Updates{},
-		// These are only relevant for version 2 artifacts
-		// We only create version 3 artifacts
-		Devices: []string{},
-		Scripts: &artifact.Scripts{},
-		// Below are unused by rdfm, but they still exist
-		MetaData:          map[string]interface{}{},
-		AugmentTypeInfoV3: &artifact.TypeInfoV3{},
-		AugmentMetaData:   map[string]interface{}{},
-		Bootstrap:         false,
-	}
-
-	return &args, nil
 }
