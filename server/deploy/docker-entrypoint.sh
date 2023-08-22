57ce271fb350be9942e41eb48a428a801744c9d5
Łukasz Kędziora <lkedziora@antmicro.com>
2023-08-22 14:42:30 +0200

    [#48235] server: deploy: Add server deployment Docker image
    Added a Dockerfile for building an RDFM server container. Additionally, added
    a simple docker-compose file for deploying a development instance of the
    server for testing purposes.

---
+++ server/deploy/docker-entrypoint.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+set +e
+
+# Print all of the missing variables at once instead of one-by-one
+_missing_variables=0
+require_variable()
+{
+	VARNAME="$1"
+	if [ -z "${!VARNAME}" ]; then
+		echo "Required environment variable missing: '${VARNAME}'"
+		_missing_variables=1
+	fi
+}
+
+server_args=""
+encrypted=1
+if [ -n "${RDFM_DISABLE_ENCRYPTION}" ]; then
+	encrypted=0
+fi
+
+if [ ${encrypted} == 1 ]; then
+	require_variable RDFM_SERVER_CERT
+	require_variable RDFM_SERVER_KEY
+	server_args="${server_args} -cert ${RDFM_SERVER_CERT}"
+	server_args="${server_args} -key ${RDFM_SERVER_KEY}"
+else
+	server_args="${server_args} -no_ssl"
+fi
+
+require_variable RDFM_JWT_SECRET
+export JWT_SECRET=${RDFM_JWT_SECRET}
+
+require_variable RDFM_HOSTNAME
+server_args="${server_args} -hostname ${RDFM_HOSTNAME}"
+
+require_variable RDFM_API_PORT
+server_args="${server_args} -http_port ${RDFM_API_PORT}"
+
+require_variable RDFM_DEVICE_PORT
+server_args="${server_args} -port ${RDFM_DEVICE_PORT}"
+
+require_variable RDFM_DB_CONNSTRING
+server_args="${server_args} -database ${RDFM_DB_CONNSTRING}"
+
+if [ ${_missing_variables} == 1 ]; then
+	echo "Cannot start server, missing required environment variables"
+	exit 1
+fi
+
+echo "Starting RDFM Management Server.."
+exec poetry run \
+	python -m rdfm_mgmt_server ${server_args}
