1553c1e40429d4c8a88d1f8bb243e1fe58ffbf9c
Illia Vysochyn <ivysochyn@antmicro.com>
2024-05-24 18:05:14 +0200

    [#59882] manager: server: scripts.py: Fix static code analysis scripts
    Reworks CI to execute python code check for Pycodestyle and MyPy
    separately.
    
    Updates scripts responsible for executing code static analysis tools to
    glob scripts recursively.
    
    Signed-off-by: Illia Vysochyn <ivysochyn@antmicro.com>

--- server/scripts.py
+++ server/scripts.py
@@ -1,10 +1,34 @@
 import subprocess
-import os
+import glob
 
-def static_checks():
-    files = [f for f in os.listdir('src') if f.endswith('.py')]
+
+def pycodestyle_checks():
+    """
+    Execute Pycodesyle checks on all python files in the src directory
+    and its subdirectories.
+
+    If any of the checks fail, an exception is raised.
+    """
+    files = [f for f in glob.glob('src/**/*.py', recursive=True)]
+    return_codes = list()
     for f in files:
-        p = subprocess.run(['python', '-m', 'pycodestyle', f'src/{f}'])
-        p.check_returncode()
-        p = subprocess.run(['python', '-m', 'mypy', f'src/{f}'])
-        p.check_returncode()
+        return_codes.append(subprocess.run(
+            ['python', '-m', 'pycodestyle', f]).returncode)
+    if any([code != 0 for code in return_codes]):
+        raise Exception('Pycodestyle static checks failed')
+
+
+def mypy_checks():
+    """
+    Execute MyPy checks on all python files in the src directory
+    and its subdirectories.
+
+    If any of the checks fail, an exception is raised.
+    """
+    files = [f for f in glob.glob('src/**/*.py', recursive=True)]
+    return_codes = list()
+    for f in files:
+        return_codes.append(subprocess.run(
+            ['python', '-m', 'mypy', f]).returncode)
+    if any([code != 0 for code in return_codes]):
+        raise Exception('MyPy static checks failed')
