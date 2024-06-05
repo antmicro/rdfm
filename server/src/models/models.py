341adb4d15674a9a1c60ccc493bacde912d1f7cc
Maciej Sobkowski <msobkowski@antmicro.com>
2024-06-05 11:34:51 +0200

    [#59157] server: alembic: add initial configuration
    The configuration was generated by `alembic init` and adjusted to use
    RDFM_DB_CONNSTRING variable to specify the database location.

---
+++ server/src/models/models.py
@@ -0,0 +1,5 @@
+from models.base import Base
+from models.package import Package
+from models.group import Group, GroupPackageAssignment
+from models.device import Device
+from models.registration import Registration
