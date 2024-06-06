9c142fad481e11165c6a95c891cdc9cb03ce4933
Maciej Sobkowski <msobkowski@antmicro.com>
2024-06-06 13:45:27 +0200

    [#59157] server: database: support creation and migration using alembic
    This commit introduces automatic handling of database migration to a new
    layout. Any future changes to database layout should also introduce a
    new migration script created with the `alembic revision --autogenerate`
    command.
    
    The database initialization mechanism takes the following cases into
    account now:
    
    - a database exists, but is not currently managed by alembic
      (`alembic_version` table is missing): it is assumed that the database
      contains the initial layout, and is marked that way using the `alembic
      stamp` command
    
    - a database does not exist, or a database exists, but the version of
      its layout is not the latest one: a migration to the latest version is
      performed using the `alembic upgrade`
    
    - a database exists and the version of its layout is the latest one: no
      action is taken
    
    That way we ensure that the database is managed by alembic and migrated
    to the latest layout no matter the initial state.

--- server/src/database/db.py
+++ server/src/database/db.py
@@ -1,8 +1,20 @@
-from sqlalchemy import create_engine, event
+from sqlalchemy import create_engine, event, inspect
 from sqlalchemy.engine import Engine
+from sqlalchemy_utils.functions import database_exists
 from models.base import Base
+from alembic import config, script, command
+from alembic.runtime import migration
+import os.path
 
 # Import all models below
+
+
+def check_current_head(alembic_cfg, connectable):
+    # type: (config.Config, engine.Engine) -> bool
+    directory = script.ScriptDirectory.from_config(alembic_cfg)
+    with connectable.begin() as connection:
+        context = migration.MigrationContext.configure(connection)
+        return set(context.get_current_heads()) == set(directory.get_heads())
 
 
 def create(connstring: str) -> Engine:
@@ -27,9 +39,38 @@ def create(connstring: str) -> Engine:
 
             event.listen(db, "connect", _fk_pragma_on_connect)
 
-        # This actually creates all the tables in the database for entities
-        # that inherit from models.device.Base
-        Base.metadata.create_all(db)
+        if os.path.isfile('alembic.ini'):
+            # If the database exists but is not managed by alembic,
+            # we assume it is the initial version and stamp it as such.
+            if (
+                database_exists(connstring) and
+                not inspect(db).has_table("alembic_version")
+            ):
+                alembic_args = [
+                    '-x', f'dbPath={connstring}',
+                    '--raiseerr',
+                    'stamp', '1',
+                ]
+                config.main(argv=alembic_args)
+
+            # Create/migrate the database
+            #
+            # If the database exists but is out of sync, it will be migrated to
+            # latest layout, if it does not exist it will be created and will
+            # go through the migrations to the most recent layout.
+            alembic_cfg = config.Config('alembic.ini')
+            if not check_current_head(alembic_cfg, db):
+                alembic_args = [
+                    '-x', f'dbPath={connstring}',
+                    '--raiseerr',
+                    'upgrade', 'head',
+                ]
+                config.main(argv=alembic_args)
+        else:
+            # Fall back to unmanaged DB if alembic.ini is missing
+            print("alembic.ini missing, using unmanaged database")
+            Base.metadata.create_all(db)
+
         return db
     except Exception as e:
         print("Database initialization failed:", repr(e))
