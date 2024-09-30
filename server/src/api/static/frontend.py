de665e17d2228ef2995722a820b2f2cb63393976
Mateusz Leonowicz <mleonowicz@antmicro.com>
2024-09-30 16:14:22 +0200

    [#66166] server: Handle more endpoints for frontend page
    Signed-off-by: Mateusz Leonowicz <mleonowicz@antmicro.com>

--- server/src/api/static/frontend.py
+++ server/src/api/static/frontend.py
@@ -1,15 +1,17 @@
 from flask import current_app
 
-from api.v1.middleware import management_read_only_api
+from api.v1.middleware import public_api
 from flask import Blueprint, send_from_directory
 
 
 frontend_blueprint: Blueprint = Blueprint("rdfm-server-frontend", __name__)
 
 
-@frontend_blueprint.route("/api/static/frontend")
-@management_read_only_api
-def get_frontend():
+@frontend_blueprint.route("/api/static/frontend", defaults={'path': ''})
+@frontend_blueprint.route("/api/static/frontend/", defaults={'path': ''})
+@frontend_blueprint.route("/api/static/frontend/<path:path>")
+@public_api
+def get_frontend(path):
     """
     Serve built frontend application from template folder.
     """
