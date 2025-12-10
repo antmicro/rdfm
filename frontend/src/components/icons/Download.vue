306670e8e043411b2ac0008a331e294f5119ca79
Anna Roszkiewicz <aroszkiewicz@antmicro.com>
2025-12-10 16:38:55 +0100

    [#88169] frontend: src: components: Add new icons
    Signed-off-by: Anna Roszkiewicz <aroszkiewicz@antmicro.com>

---
+++ frontend/src/components/icons/Download.vue
@@ -0,0 +1,19 @@
+<!--
+Copyright (c) 2025 Antmicro <www.antmicro.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
+
+<template>
+    <svg width="20" height="20" viewBox="1 1 17 17" xmlns="http://www.w3.org/2000/svg">
+        <path
+            d="M10 13L6 9L7.0625 7.9375L9.25 10.125V3H10.75V10.125L12.9375 7.9375L14 9L10 13ZM5.49417 16C5.08139 16 4.72917 15.8531 4.4375 15.5594C4.14583 15.2656 4 14.9125 4 14.5V13H5.5V14.5H14.5V13H16V14.5C16 14.9125 15.8531 15.2656 15.5592 15.5594C15.2653 15.8531 14.9119 16 14.4992 16H5.49417Z"
+        />
+    </svg>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Download',
+};
+</script>
--- frontend/src/components/packages/PackagesList.vue
+++ frontend/src/components/packages/PackagesList.vue
@@ -159,16 +159,7 @@ Component wraps functionality for displaying and working with rdfm packages.
                                                     )
                                             "
                                         >
-                                            <svg
-                                                width="20"
-                                                height="20"
-                                                viewBox="1 1 17 17"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path
-                                                    d="M10 13L6 9L7.0625 7.9375L9.25 10.125V3H10.75V10.125L12.9375 7.9375L14 9L10 13ZM5.49417 16C5.08139 16 4.72917 15.8531 4.4375 15.5594C4.14583 15.2656 4 14.9125 4 14.5V13H5.5V14.5H14.5V13H16V14.5C16 14.9125 15.8531 15.2656 15.5592 15.5594C15.2653 15.8531 14.9119 16 14.4992 16H5.49417Z"
-                                                />
-                                            </svg>
+                                            <Download />
                                             Direct download
                                         </button>
                                         <button
@@ -180,17 +171,7 @@ Component wraps functionality for displaying and working with rdfm packages.
                                                     )
                                             "
                                         >
-                                            <svg
-                                                width="20"
-                                                height="20"
-                                                viewBox="1 1 17 17"
-                                                fill="none"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path
-                                                    d="M9 14H6C4.89333 14 3.95 13.6095 3.17 12.8285C2.39 12.0477 2 11.1033 2 9.99521C2 8.88729 2.39 7.94444 3.17 7.16667C3.95 6.38889 4.89333 6 6 6H9V7.5H6C5.30556 7.5 4.71528 7.74306 4.22917 8.22917C3.74306 8.71528 3.5 9.30556 3.5 10C3.5 10.6944 3.74306 11.2847 4.22917 11.7708C4.71528 12.2569 5.30556 12.5 6 12.5H9V14ZM7 10.75V9.25H13V10.75H7ZM11 14V12.5H14C14.6944 12.5 15.2847 12.2569 15.7708 11.7708C16.2569 11.2847 16.5 10.6944 16.5 10C16.5 9.30556 16.2569 8.71528 15.7708 8.22917C15.2847 7.74306 14.6944 7.5 14 7.5H11V6H14C15.1067 6 16.05 6.39049 16.83 7.17146C17.61 7.95229 18 8.89674 18 10.0048C18 11.1127 17.61 12.0556 16.83 12.8333C16.05 13.6111 15.1067 14 14 14H11Z"
-                                                />
-                                            </svg>
+                                            <CopyLink />
                                             Copy link
                                         </button>
                                     </div>
@@ -309,6 +290,8 @@ import { POLL_INTERVAL, useNotifications, allowedTo, hasUploadAccess } from '../
 import BlurPanel from '../BlurPanel.vue';
 import RemovePopup from '../RemovePopup.vue';
 import TitleBar from '../TitleBar.vue';
+import Download from '../icons/Download.vue';
+import CopyLink from '../icons/CopyLink.vue';
 import {
     packageResources,
     groupsResources,
@@ -333,6 +316,8 @@ export default {
         TitleBar,
         CaretDown,
         CaretUp,
+        Download,
+        CopyLink,
     },
     setup() {
         let intervalID: undefined | number = undefined;
