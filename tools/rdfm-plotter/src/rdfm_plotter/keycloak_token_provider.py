b91326342e34bbaf57479861129d32e6c5e00f64
Kacper Zienkiewicz <kzienkiewicz@antmicro.com>
2026-04-03 15:32:15 +0200

    [#87864] tools: rdfm-plotter: Use authlib in KeycloakTokenProvider

--- tools/rdfm-plotter/src/rdfm_plotter/keycloak_token_provider.py
+++ tools/rdfm-plotter/src/rdfm_plotter/keycloak_token_provider.py
@@ -1,48 +1,45 @@
 import datetime
 import requests
 import json
+import time
 from kafka.sasl.oauth import AbstractTokenProvider
+from authlib.oauth2.rfc6749.wrappers import OAuth2Token
+from authlib.integrations.requests_client import OAuth2Session
 from typing import Optional
 
 
 class KeycloakTokenProvider(AbstractTokenProvider):
-    _token: Optional[str]
-    acquire: Optional[datetime.datetime]
+    _token = Optional[OAuth2Token]
+    _client_id: str
+    _client_secret: str
+    _token_endpoint: str
+    _audience: str
+    _cafile: Optional[str]
+    _leeway: int
 
     def __init__(self,
-                 id: str,
-                 secret: str,
-                 url: str,
+                 client_id: str,
+                 client_secret: str,
+                 token_endpoint: str,
                  audience: str,
-                 reauth_margin: int = 15) -> None:
-        self.id = id
-        self.secret = secret
-        self.audience = audience
-        self.url = url
-        self.reauth = reauth_margin
-        self.acquire = None
+                 cacert: Optional[str] = None,
+                 leeway: int = 30) -> None:
+        self._client_id = client_id
+        self._client_secret = client_secret
+        self._token_endpoint = token_endpoint
+        self._audience = audience
         self._token = None
+        self._cacert = cacert
+        self._leeway = leeway
 
-    def token(self) -> Optional[str]:
-        if self._token is None or self.acquire is None:
-            self._token = self.fetch()
-        elif self.acquire < datetime.datetime.now():
-            self._token = self.fetch()
-        return self._token
-
-    def fetch(self) -> Optional[str]:
-        data = {
-            "grant_type": "urn:ietf:params:oauth:grant-type:uma-ticket",
-            "client_id": self.id,
-            "client_secret": self.secret,
-            "audience": self.audience
-        }
-        response = requests.post(self.url, data=data)
-        if 200 == response.status_code:
-            r = json.loads(response.text)
-            expires = r["expires_in"]
-            now = datetime.datetime.now()
-            self.acquire = (now + datetime.timedelta(seconds=expires - self.reauth)
-                            if expires > self.reauth else now)
-            return str(r["access_token"]) if r["access_token"] else None
-        return None
+    def token(self) -> str:
+        if self._token and isinstance(self._token, OAuth2Token):
+            if self._token["expires_at"] > time.time() + self._leeway:
+                return self._token["access_token"]
+        client = OAuth2Session(client_id=self._client_id, client_secret=self._client_secret)
+        if self._cacert:
+            client.verify = self._cacert
+        self._token = client.fetch_token(self._token_endpoint,
+                                         grant_type="urn:ietf:params:oauth:grant-type:uma-ticket",
+                                         audience=self._audience)
+        return self._token["access_token"]
